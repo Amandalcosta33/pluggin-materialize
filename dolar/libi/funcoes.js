37f91b056ee4d7d96e65a3261c7496a87b470df7
Amanda.Lopes <amanda.lcosta33@gmail.com>
2020-05-20 22:10:25 -0300

    feito cep e cotação do dolar

---
+++ dolar/libi/funcoes.js
@@ -0,0 +1,16 @@
+$('#botao-moeda').click(function(){
+    let moeda = $('#moeda').val();
+    $('#resultado').html('');
+    $.ajax({
+        url: 'https://economia.awesomeapi.com.br/json/' + moeda,
+        success: function(dados){
+            $('#resultado').append("Moeda: " + dados[0].name);
+            $('#resultado').append(" <br> Cotação em R$: " + dados[0].high);
+            
+        },
+        error: function(){
+            $('#resultado').html('Resultado não encontrado');
+        }
+        
+    });
+});
